52909aaf23168485b45bc1d2397436189e9dfc56
Dao Chien <23a1001d0048@students.hou.edu.vn>
2025-06-30 20:51:44 +0700

    create User
    Signed-off-by: Dao Chien <23a1001d0048@students.hou.edu.vn>

---
+++ app/src/main/java/DoiTuong/User.java
@@ -0,0 +1,136 @@
+package DoiTuong;
+
+public class User {
+    private String maUser;
+    private String email;
+    private String tenHienThi;
+    private String soDienThoai;
+    private String anhDaiDien;
+    private String tieySu;
+    private String ngaySinh;
+    private String khoaVien;
+    private String maSinhVien;
+    private boolean daXacThucEmail;
+
+    public User() {
+    }
+
+    // Constructor
+    public User(String maUser, String email, String tenHienThi, String soDienThoai,
+                String anhDaiDien, String tieySu, String ngaySinh, String khoaVien,
+                String maSinhVien, boolean daXacThucEmail) {
+        this.maUser = maUser;
+        this.email = email;
+        this.tenHienThi = tenHienThi;
+        this.soDienThoai = soDienThoai;
+        this.anhDaiDien = anhDaiDien;
+        this.tieySu = tieySu;
+        this.ngaySinh = ngaySinh;
+        this.khoaVien = khoaVien;
+        this.maSinhVien = maSinhVien;
+        this.daXacThucEmail = daXacThucEmail;
+    }
+
+    // Constructor với thông tin cơ bản
+    public User(String maUser, String email, String tenHienThi, String anhDaiDien) {
+        this.maUser = maUser;
+        this.email = email;
+        this.tenHienThi = tenHienThi;
+        this.anhDaiDien = anhDaiDien;
+        this.daXacThucEmail = false;
+    }
+
+    // Getters và Setters
+    public String getMaUser() {
+        return maUser;
+    }
+
+    public void setMaUser(String maUser) {
+        this.maUser = maUser;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTenHienThi() {
+        return tenHienThi;
+    }
+
+    public void setTenHienThi(String tenHienThi) {
+        this.tenHienThi = tenHienThi;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getAnhDaiDien() {
+        return anhDaiDien;
+    }
+
+    public void setAnhDaiDien(String anhDaiDien) {
+        this.anhDaiDien = anhDaiDien;
+    }
+
+    public String getTieySu() {
+        return tieySu;
+    }
+
+    public void setTieySu(String tieySu) {
+        this.tieySu = tieySu;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getKhoaVien() {
+        return khoaVien;
+    }
+
+    public void setKhoaVien(String khoaVien) {
+        this.khoaVien = khoaVien;
+    }
+
+    public String getMaSinhVien() {
+        return maSinhVien;
+    }
+
+    public void setMaSinhVien(String maSinhVien) {
+        this.maSinhVien = maSinhVien;
+    }
+
+    public boolean isDaXacThucEmail() {
+        return daXacThucEmail;
+    }
+
+    public void setDaXacThucEmail(boolean daXacThucEmail) {
+        this.daXacThucEmail = daXacThucEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "maUser='" + maUser + '\'' +
+                ", email='" + email + '\'' +
+                ", tenHienThi='" + tenHienThi + '\'' +
+                ", soDienThoai='" + soDienThoai + '\'' +
+                ", anhDaiDien='" + anhDaiDien + '\'' +
+                ", khoaVien='" + khoaVien + '\'' +
+                ", maSinhVien='" + maSinhVien + '\'' +
+                '}';
+    }
+}
