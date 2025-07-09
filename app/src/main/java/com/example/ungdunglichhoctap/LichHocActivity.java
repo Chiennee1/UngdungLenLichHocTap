0e6a3ac354df787cf685f817c2037557249681f7
Dao Chien <23a1001d0048@students.hou.edu.vn>
2025-07-09 15:36:44 +0700

    update SettingActivity
    Signed-off-by: Dao Chien <23a1001d0048@students.hou.edu.vn>

---
+++ app/src/main/java/com/example/ungdunglichhoctap/LichHocActivity.java
@@ -0,0 +1,18 @@
+package com.example.ungdunglichhoctap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LichHocActivity extends AppCompatActivity {
+    // This activity can be used to display the schedule of classes or lessons.
+    // You can add UI elements in the corresponding XML layout file (activity_lich_hoc.xml) and implement functionality here.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lichhoc);
+
+        // Initialize UI elements and set up click listeners if needed
+    }
+}
